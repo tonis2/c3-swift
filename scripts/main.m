85fb358e82768e50e04ecb4f2d976ecb60bf06e9
tanton <tanton@paysure.solutions>
2024-11-16 11:12:27 +0200

    Update building

--- scripts/main.m
+++ scripts/main.m
@@ -1,26 +1,37 @@
 #import <Cocoa/Cocoa.h>
 
-void create_window() {
-    @autoreleasepool {
-        // Create a default window
-        NSWindow *window = [[NSWindow alloc] init];
-        // Make it blue just for better visibility
-        [window setBackgroundColor:[NSColor blueColor]];
-        [window setFrame:NSMakeRect(500.0f, 500.0f, 800.0f, 400.f) display:YES animate:YES];
-        // Bring to front and make it key
-        [window makeKeyAndOrderFront:nil];
-        // Custom run loop
-        NSEvent* event;
-        while(1) {
-            do {
-                event = [window nextEventMatchingMask:NSEventMaskAny]; //window shows now
-                if ([event type] == NSEventTypeLeftMouseDown) {
-                    NSLog(@"Mouse down");
-                }
-                else {
-                    NSLog(@"Something happened");
-                }
-            } while (event != nil);
-        }
+typedef struct Window_Params
+{
+    int x;
+    int y;
+    int width;
+    int height;
+} Window_Params;
+
+NSUInteger windowStyle = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable;
+
+void create_window(Window_Params params) {
+    NSRect frame = NSMakeRect(params.x, params.y, params.width, params.height);
+
+    NSWindow *window = [[NSWindow alloc] initWithContentRect:frame styleMask:windowStyle backing:NSBackingStoreBuffered defer:NO];
+    // Make it blue just for better visibility
+    [window setBackgroundColor:[NSColor blueColor]];
+    // Bring to front and make it key
+    [window makeKeyAndOrderFront:nil];
+    [window setTitle: [NSString stringWithUTF8String:"Mac window"]];
+    // Custom run loop
+    NSEvent* event;
+    while(1) {
+        do {
+            event = [window nextEventMatchingMask:NSEventMaskAny]; //window shows now
+            if ([event type] == NSEventTypeLeftMouseDown) {
+                NSLog(@"Mouse down");
+            }
+            else {
+                NSLog(@"Something happened");
+            }
+        } while (event != nil);
     }
+
+    [window autorelease];
 }
